86c0eb975bb86610560cb513d73a479f8231fef0
Alex Chiriac <cristian.chiriac02@stud.acs.upb.ro>
2024-04-23 18:03:45 +0300

    Implement functions
    Signed-off-by: Alex Chiriac <cristian.chiriac02@stud.acs.upb.ro>

--- tree.py
+++ tree.py
@@ -62,27 +62,31 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Method for erasing a tree """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """ Method for printing a tree """
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """ Method for printing a tree in inorder """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """ Method for printing a tree in preorder """
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
     def _printPostorderTree(self, node):
-        # TODO 2
-        pass
-
-
+        """ Method for printing a tree in postorder """
+        if node is not None:
+            self._printPostorderTree(node.left)
+            self._printPostorderTree(node.right)
+            print(str(node.data) + ' ')
